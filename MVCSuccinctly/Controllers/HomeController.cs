3fda67571a2253429194ea83d834fe6f22181155
bigheadted <tedchaloux@hotmail.com>
2016-01-13 19:58:34 -0800

    first commit

---
+++ MVCSuccinctly/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MVCSuccinctly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSuccinctly.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new GreetingModel
+            {
+                Greeting = "Welcome to the World of MVC",
+                Name = "Your friendly neighborhood MVC App"
+            };
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCSuccinctly/Controllers/ItineraryController.cs
@@ -0,0 +1,40 @@
+﻿using MVCSuccinctly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSuccinctly.Controllers
+{
+    public class ItineraryController : Controller
+    {
+        //
+        // GET: /Itinerary/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create(ItineraryItem data) 
+        {
+            if (ModelState.IsValid)
+            {
+                // Handle saving the data only if 
+                // the model state is valid 
+            }
+            else 
+            { 
+                ModelState.AddModelError("", "The data you entered was not valid"); 
+            }
+
+
+
+            return View(); 
+        }
+
+ 
+
+    }
+}
